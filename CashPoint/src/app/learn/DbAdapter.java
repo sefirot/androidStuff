a6e23a2839213a99a85b5f20dc6ae1a9f4c61544
Lotharla <lotharlanger@yahoo.com>
2012-01-12 23:39:33 +0100

    12jan12 : distribution + test

--- CashPoint/src/app/learn/DbAdapter.java
+++ CashPoint/src/app/learn/DbAdapter.java
@@ -1,6 +1,6 @@
 package app.learn;
 
-import java.util.Date;
+import java.util.*;
 import java.text.SimpleDateFormat;
 
 import android.content.ContentValues;
@@ -17,7 +17,7 @@ public class DbAdapter extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "data";
     private static final String DATABASE_TABLE = "cashpoint";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 1;
 	
 	public DbAdapter(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -27,7 +27,7 @@ public class DbAdapter extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL("create table " + DATABASE_TABLE + " (" +
-			" entry integer not null," +	//	unique for the inputs, reference for the distributions
+			" entry integer not null," +	//	unique for the entries, reference for the distributions
 			" name text not null," +		//	the person involved
 			" amount real not null," +		//	the amount of money, negative for distributions
 			" currency text," +				//	if null it's the default currency (Euro or Dollar or ...)
@@ -42,12 +42,12 @@ public class DbAdapter extends SQLiteOpenHelper {
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
                 + newVersion + ", which will destroy all old data");
-		recreate(db);
 		
+		recreate(db);
 	}
     
     public void recreate(SQLiteDatabase db) {
-    	db.execSQL("DROP TABLE IF EXISTS cashpoint");
+    	db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
     	onCreate(db);
     }
 	
@@ -56,6 +56,24 @@ public class DbAdapter extends SQLiteOpenHelper {
     public SQLiteDatabase getDb() {
 		return mDb;
 	}
+
+    String[] getFieldNames() {
+    	return new String[] {"entry", "name", "amount", "currency", "date", "comment"};
+    }
+    
+    ContentValues putValues(int entry, String name, float amount, String currency, String timeStamp, String comment) {
+        ContentValues values = new ContentValues();
+        
+    	String[] fields = getFieldNames();
+        values.put(fields[0], entry);
+        values.put(fields[1], name);
+        values.put(fields[2], amount);
+        values.put(fields[3], currency);
+        values.put(fields[4], timeStamp);
+        values.put(fields[5], comment);
+        
+        return values;
+    }
 
 	public int getNewEntryId() {
         Cursor cursor = mDb.rawQuery("select max(entry) from " + DATABASE_TABLE, null);
@@ -70,38 +88,57 @@ public class DbAdapter extends SQLiteOpenHelper {
     
     private SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     
-    public String getDateFormat(Date date) {
+    public String getTimeStamp(Date date) {
 		return mDateFormat.format(date);
 	}
-
-	public int createEntry(String name, double amount, String currency, String comment) {
+    
+	public int addEntry(String name, float amount, String currency, String comment) {
     	int entryId = getNewEntryId();
-    	
-        ContentValues values = new ContentValues();
-        values.put("entry", "" + entryId);
-        values.put("name", name);
-        values.put("amount", amount);
-        values.put("currency", currency);
-        values.put("date", getDateFormat(new Date()));
-        values.put("comment", comment);
-        long rowId = mDb.insert(DATABASE_TABLE, null, values);
+        
+        long rowId = mDb.insert(DATABASE_TABLE, null, 
+        		putValues(entryId, name, amount, 
+                	currency, getTimeStamp(new Date()), comment));
         if (rowId < 0)
         	return -1;
-        else {
+        else 
         	return entryId;
-        }
     }
     
-    public Cursor fetchEntry(int entry) throws SQLException {
-
-        Cursor mCursor = mDb.query(true, DATABASE_TABLE, 
-        		new String[] {"entry", "name", "amount", "currency", "date", "comment"}, 
-        		"entry=" + entry, 
+    public Cursor fetchEntry(int entryId) {
+        Cursor cursor = mDb.query(true, DATABASE_TABLE, 
+        		getFieldNames(), 
+        		"entry=" + entryId, 
         		null, null, null, null, null);
-        if (mCursor != null) {
-            mCursor.moveToFirst();
-        }
-        return mCursor;
-
+        if (cursor != null) 
+            cursor.moveToFirst();
+        return cursor;
     }
+    
+    public boolean doDistribution(int entryId, Map<String, Number> shares) {
+    	Cursor cursor = fetchEntry(entryId);
+    	if (cursor == null)
+    		return false;
+    	
+    	String entrant = cursor.getString(cursor.getColumnIndex("name"));
+    	float amount = cursor.getFloat(cursor.getColumnIndex("amount"));
+    	String currency = cursor.getString(cursor.getColumnIndex("currency"));
+    	
+    	for (String name : shares.keySet()) 
+    		if (!entrant.equals(name)) {
+    			float share = shares.get(name).floatValue();
+    			
+    			if (mDb.insert(DATABASE_TABLE, null, 
+    					putValues(entryId, name, -share, currency, null, null)) < 0)
+    	        	return false;
+    	        
+    			amount -= share;
+    		}
+
+    	if (mDb.insert(DATABASE_TABLE, null, 
+    		   putValues(entryId, entrant, -amount, currency, null, null)) < 0)
+        	return false;
+   	
+    	return true;
+    }
+
 }
--- CashPointTest/src/app/learn/test/CashPointTest.java
+++ CashPointTest/src/app/learn/test/CashPointTest.java
@@ -1,58 +1,64 @@
 package app.learn.test;
 
-import java.util.Date;
+import java.util.*;
+
+import android.test.*;
+import android.database.Cursor;
 
 import app.learn.*;
 
-import android.test.*;
-import android.widget.TextView;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-
-public class CashPointTest extends ActivityInstrumentationTestCase2<CashPoint> {		//	AndroidTestCase
+public class CashPointTest extends ActivityInstrumentationTestCase2<CashPoint> {
 	public CashPointTest() {
 		super("app.learn", CashPoint.class);
 	}
 	
     private CashPoint mActivity;
-    private TextView mTextView;
-    private String resourceString;
-    
+    private DbAdapter dbAdapter;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         mActivity = this.getActivity();
+    	dbAdapter = new DbAdapter(mActivity);
     }
+
+    @Override
+    protected void tearDown() throws Exception {
+		dbAdapter.close();
+        super.tearDown();
+	}
     
-    public void testPreconditions() {
-        mTextView = (TextView) mActivity.findViewById(app.learn.R.id.nameLabel);
-    	assertNotNull(mTextView);
-    }
-    
-    public void testText() {
-        resourceString = mActivity.getString(app.learn.R.string.nameLabelText);
-    	assertEquals(resourceString,(String)mTextView.getText());
-    }
-    
-    public void testDbInOut() {
-    	DbAdapter dbAdapter = new DbAdapter(mActivity);
+    public void testEntry() {
+    	int entryId = dbAdapter.addEntry("Bob", 100.5f, null, "gas");
+    	assertEquals(dbAdapter.getNewEntryId() - 1, entryId);
     	
-    	int entry = dbAdapter.createEntry("Bob", 100.5, null, "gas");
-    	assertEquals(dbAdapter.getNewEntryId() - 1, entry);
-    	
-    	Cursor outputCursor = dbAdapter.fetchEntry(entry);    	
+    	Cursor outputCursor = dbAdapter.fetchEntry(entryId);    	
 		assertEquals("Bob", outputCursor.getString(1));
-		assertEquals("" + 100.5, outputCursor.getString(2));
+		assertEquals(100.5f, outputCursor.getFloat(2));
 		assertEquals(null, outputCursor.getString(3));
-		String now = dbAdapter.getDateFormat(new Date());
+		String now = dbAdapter.getTimeStamp(new Date());
 		assertEquals(now.substring(0, 16), 
 				outputCursor.getString(4).substring(0, 16));
 		assertEquals("gas", outputCursor.getString(5));
 		outputCursor.close();
+    }
+    
+    public void testDistribution() {
+    	float amount = 100.2f;
+    	int entryId = dbAdapter.addEntry("Bob", amount, null, "gas");
+    	HashMap<String, Number> shares = new HashMap<String, Number>();
+    	shares.put("Sue", amount / 3);
+    	shares.put("Tom", amount / 3);
+    	assertTrue(dbAdapter.doDistribution(entryId, shares));
     	
-		dbAdapter.close();
-    	
+    	Cursor cursor = dbAdapter.fetchEntry(entryId);
+		assertEquals(4, cursor.getCount());
+		float sum = 0f;
+		do {
+			sum += cursor.getFloat(cursor.getColumnIndex("amount"));
+		} while (cursor.moveToNext());
+		assertFalse("distribution leaks", Math.abs(sum) > 0.001);
+		cursor.close();
     }
     
 }
